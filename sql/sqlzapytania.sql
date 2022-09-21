37a918212a939da4636a116e3c30db3ab4447087
czarus99 <cezarprokoks@gmail.com>
2022-09-21 11:25:21 +0200

    Bazadanych

--- sql/sqlzapytania.sql
+++ sql/sqlzapytania.sql
@@ -13,3 +13,7 @@ SELECT * FROM `resources_has_buildings`;
 SELECT * FROM `terrain`;
 SELECT * FROM `terrain_has_buildings`;
 SELECT * FROM `unit`;
+SELECT * FROM `player`;
+SELECT Name FROM `player`  JOIN Player_has_Army ON player.id=player_has_army.Player_id; 
+SELECT Max_Size FROM `army` JOIN player_has_army ON army.id=player_has_army.Army_id JOIN player ON player.id=player_has_army.Player_id;
+SELECT Rarity FROM `resources` JOIN player_has_resources ON resources.id=player_has_resources.Resources_id JOIN player ON player.id=player_has_resources.Player_id 
